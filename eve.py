359e7b58f22cea09ab6ff1cb82aa8a8910b2e999
tsubota-kouga <kouga.infini@gmail.com>
2020-03-12 19:01:23 +0900

    changed model

--- eve.py
+++ eve.py
@@ -4,38 +4,21 @@ from torch.optim import Optimizer
 import math
 
 class Eve(Optimizer):
-    '''
-    Implementation of Eve(https://arxiv.org/pdf/1611.01505.pdf)
-
-    example:
-    ```python
-    model = HogeModel()
-    criterion = nn.L1loss()
-    optimizer = Eve(model.parameters())
-
-    optimizer.zero_grad()
-    input = torch.rand((2, 3))
-    output = model(input)
-    loss = criterion(output)
-    loss.backward()
-    optimizer.step(lambda: loss)  # Note: lambda expression is required
-    ```
-    '''
     def __init__(self,
                  params,
                  lr=1e-3,
                  betas=(0.9, 0.999, 0.999),
-                 c=10.0,
                  eps=1e-8,
-                 f_star=0.0,
+                 k=0.1,
+                 K=10.0,
                  weight_decay=0,
                  amsgrad=False):
         defaults = dict(
             lr=lr,
             betas=betas,
-            c=c,
             eps=eps,
-            f_star=f_star,
+            k=k,
+            K=K,
             weight_decay=weight_decay,
             amsgrad=amsgrad)
         if lr <= 0.0:
@@ -43,8 +26,6 @@ class Eve(Optimizer):
         if not (0.0 <= betas[0] < 1.0) and \
            not (0.0 <= betas[1] < 1.0) and \
            not (0.0 <= betas[2] < 1.0):
-            raise ValueError
-        if c < 1.0:
             raise ValueError
         if eps <= 0.0:
             raise ValueError
@@ -60,7 +41,7 @@ class Eve(Optimizer):
         f = None
         if closure is not None:
             loss = closure()
-            f = loss.data
+            f = loss.data.item()
         for group in self.param_groups:
             for p in group["params"]:
                 if p.grad is None:
@@ -69,9 +50,9 @@ class Eve(Optimizer):
 
                 lr = group["lr"]
                 beta1, beta2, beta3 = group["betas"]
-                c = group["c"]
+                k = group["k"]
+                K = group["K"]
                 eps = group["eps"]
-                f_star = group["f_star"]
                 amsgrad = group["amsgrad"]
 
                 state = self.state[p]
@@ -84,16 +65,18 @@ class Eve(Optimizer):
                     state["v"] = torch.zeros_like(
                         p.data,
                         memory_format=torch.preserve_format)
+                    state["d"] = 1.0
                     if amsgrad:
                         state["max_v"] = torch.zeros_like(
                             p.data,
                             memory_format=torch.preserve_format)
+                    state["f_hat"] = 0.0
 
                 step = state["step"] + 1
                 state["step"] = step
 
-                bias_correction1 = 1 - math.power(beta1, step)
-                bias_correction2 = 1 - math.power(beta2, step)
+                bias_correction1 = 1 - beta1 ** step
+                bias_correction2 = 1 - beta2 ** step
 
                 if group["weight_decay"] != 0:
                     g.add_(group["weight_decay"], p.data)
@@ -104,28 +87,33 @@ class Eve(Optimizer):
                 v = state["v"]
                 v.mul_(beta2).addcmul_(1 - beta2, g, g)
 
-                d_tilde = None
+                d = None
                 if step > 1:
-                    prev_f = state["f"]
-                    state["f"] = f
-                    d = torch.abs(f - prev_f) / (torch.min(f, prev_f) - f_star)
-                    d_hat = d.clamp(1 / c, c)
-                    d_tilde = beta3 * state["d_tilde"] + (1 - beta3) * d_hat
-                    state["d_tilde"] = d_tilde
+                    if f < state["f_hat"]:
+                        delta = k + 1
+                        Delta = K + 1
+                    else:
+                        delta = 1.0 / (K + 1)
+                        Delta = 1.0 / (k + 1)
+                    c = min(max(delta, f / state["f_hat"]), Delta)
+                    next = c * state["f_hat"]
+                    r = np.abs(next - state["f_hat"]) / min(next, state["f_hat"])
+                    d = beta3 * state["d"] + (1 - beta3) * r
+                    state["d"] = d
+                    state["f_hat"] = next
                 else:
-                    state["f"] = f
-                    d_tilde = 1.0
-                    state["d_tilde"] = d_tilde
+                    state["f_hat"] = f
+                    state["d"] = 1.0
+                    d = 1.0
 
-                v = v if not amsgrad else torch.max(
-                        state["max_v"], v, out=state["max_v"])
-                p.data.addcdiv_(
-                    -lr if d_tilde == 1.0 else -lr / d_tilde,
-                    m * math.sqrt(bias_correction2),
-                    v.sqrt()
-                     .add(eps, math.sqrt(bias_correction2))
-                     .mul(bias_correction1)
-                    )
+                if amsgrad:
+                    max_v = state["max_v"]
+                    torch.max(state["max_v"], v, out=max_v)
+                    denom = (max_v.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+                else:
+                    denom = (v.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+                step_size = lr / (bias_correction1 * d)
 
+                p.data.addcdiv_(-step_size, m, denom)
         return loss
 
